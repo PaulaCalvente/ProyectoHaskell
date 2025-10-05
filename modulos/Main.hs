b0973b2a622d908939109ac0aba350d2c4de0ea6
Aleholguin12 <aleholguin124@gmail.com>
2025-10-05 10:14:18 +0200

    commit 05/10/25

--- modulos/Main.hs
+++ modulos/Main.hs
@@ -3,6 +3,22 @@ module Main where
 import Types
 import Collision
 import World
+import BotAction
+import Movement
+
+-- Bot de ejemplo: recibe el mundo y su propio Id, devuelve un comando
+exampleBot :: World -> Id -> BotCmd -- Funcion que si detecta un robot en su zona le dispara, si no patruya unos puntos definidos
+exampleBot world myId
+  | not (null nearbyEnemies) = ShootAt (TRobot (idR (head nearbyEnemies))) -- Cogemos al primer robot de la lista de cercanos, con idR
+                                                                           -- sacamos su ID de robot, con TRobot lo apuntamos (esta
+                                                                           -- definido en BotAction y con ShootAt le disparamos)
+  | otherwise                = Patrol [(0,0), (50,0), (50,50), (0,50)]
+  where
+    myRobot = head [r | r <- robots world, idR r == myId]
+    nearbyEnemies = [ r | r <- robots world -- Busco los robots del mundo
+                        , idR r /= myId -- No soy yo
+                        , isRobotAlive r -- Está vivo
+                        , detectedAgent myRobot r ] -- Lo detecto con mi radar
 
 
 main :: IO ()
